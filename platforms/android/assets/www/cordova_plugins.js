02b711ed69797937ad3fd6bb44f606db7e0f77b7
Vicki Long <longv@bxscience.edu>
2014-08-27 17:00:50 -0400

    Added social media buttons to profile.html and linked to browser

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -27,12 +27,20 @@ module.exports = [
         "clobbers": [
             "CameraPopoverHandle"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.camera": "0.3.1"
+    "org.apache.cordova.camera": "0.3.1",
+    "org.apache.cordova.inappbrowser": "0.5.1"
 }
 // BOTTOM OF METADATA
 });
